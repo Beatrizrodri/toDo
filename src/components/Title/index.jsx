c321a446450db53d355172415df7809ca229d89b
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-10-13 10:51:54 -0300

    add title component

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import '../../styles/global.css';
+
+export default function Title() {
+  return (
+    <div id="title-content">
+      <h1>To Do App</h1>
+      <span>Para você organizar seu dia com facilidade</span>
+    </div>
+  );
+}
