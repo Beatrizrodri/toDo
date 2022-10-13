4c0885e11cb7ca529aff8284b348607ef71c0927
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-10-13 10:52:51 -0300

    add task component

---
+++ src/components/Task/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Task() {
+  return (
+    <div className="task-container">
+      <div className="task">
+        <span className="task-value">Sua tarefa aqui</span>
+      </div>
+    </div>
+  );
+}
