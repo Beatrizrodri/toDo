154baf2a8f78c14314cdaae80fe34941f562ecbf
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-10-13 10:52:32 -0300

    add AddButton component

---
+++ src/components/AddButton/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function AddButton() {
+  return (
+    <div id="content-add-button">
+      <input type="text" placeholder="Digite aqui sua tarefa" />
+      <button type="submit">Adicionar</button>
+    </div>
+  );
+}
